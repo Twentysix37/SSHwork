36dbd210be4e2cdd1a55c7e93148ca251b027904
Twentysix37 <Twentysix37@gmail.com>
2024-10-18 16:13:39 -0400

    Workin out issues with Path

--- sshwork/src/main.rs
+++ sshwork/src/main.rs
@@ -4,28 +4,35 @@ use core::error;
 use std::fs::{read_dir, read_to_string as read_file, remove_file, DirEntry}; 
 // Filesystem utils.
 use std::path::{self, Path}; // Path to entry in filesystem.
-use std::process::Command; // Child process runner.
+use std::process::{Child, Command}; // Child process runner.
+use std::io::{self, BufRead};
+use std::io::Write;
 
 
-const SSH_DIR: &str = "~/.ssh";
+
+const SSH_DIR: &str = "%HOMEPROFILE%/.ssh";
 
 type Error = String;
 type IsInstalled = bool;
 
 fn get_ssh_info() -> Result<IsInstalled, Error> {
+    println!("{SSH_DIR}");
     let mut ssh_installed = false;
     let mut ssh_checks = 0;
     // Check that each client-side SSH binary is installed; forward any I/O errors.
     //todo!("refactor the following into a `for`-loop and run for each client-side binary: `ssh`, `ssh-keygen`, `ssh-agent`, `ssh-add`");
-    let bins = vec! ["ssh","ssh-keygen","ssh-agent","ss-add"];
+    let bins = vec! ["ssh","ssh-keygen","ssh-agent","ssh-add" ];
     for bin in bins.iter() {
     //let bin = "ssh";
-    let child_output= Command::new("which").arg(bin).output().map_err(|err| {
-        format!("failed to run process: `which {bin}`, error: `{err}`")
+    let child_output= Command::new("where")
+    .arg(bin)
+    .output()
+    .map_err(|err| {
+        format!("failed to run process: `where {bin}`, error: `{err}`")
     })?;
     if child_output.status.success() {
         let child_stdout = String::from_utf8(child_output.stdout)
-            .map_err(|err| format!("failed to parse process stdout: `which {bin}`, error: `{err}`"))?;
+        .map_err(|err| format!("failed to parse process stdout: `where {bin}`, error: `{err}`"))?;
         let bin_path = child_stdout.trim();
         println!("[✓] {bin}: found ({bin_path})");
         //ssh_installed = true;
@@ -37,16 +44,31 @@ fn get_ssh_info() -> Result<IsInstalled, Error> {
 
     if ssh_checks == 4{
         
-        let ssh_version = Command::new("ssh").arg("-V")
-            .output()
+        let ssh_version = Command::new("ssh")
+            .arg("-V")
+            .output() //output()
             .map_err(|error| {
                 format!("failed to run process: `ssh -V`, error: `{error}`")
             })?;
-            println!("SSH version {}",ssh_version.status);
-            let ssh_path = Path::new(SSH_DIR);
-            if ssh_path.exists(){
+            
+        if ssh_version.status.success(){
+            
+            //println!("status: {}", String::from_utf8_lossy(&ssh_version.stdout));
+            println!("status: {}", ssh_version.status);
+            
+
+            let sshv_output = String::from_utf8(ssh_version.stderr)
+            .map_err(|error| format!("failed to parse process stdout: `ssh -V`, error: `{error}`"))?;
+            let sshv_out = sshv_output.trim();
+            println!("SSH version {sshv_out}");
+            
+            let ssh_path = Path::new("%HOMEPROFILE%/.ssh");
+            let sp:std::borrow::Cow<'_, str> = ssh_path.to_string_lossy();
+            println!("{sp}");
+            
+            if ssh_path.try_exists().is_ok(){
                 println!("[✓] SSH directory: found");
-                let ssh_config = Path::new("~/.ssh/config");
+                let ssh_config = Path::new("%HOMEPROFILE%/.ssh/config");
                 if ssh_config.exists(){
                     println!("[✓] SSH config file: found");
                     ssh_installed = true
@@ -56,6 +78,9 @@ fn get_ssh_info() -> Result<IsInstalled, Error> {
             } else {
                 println!("[✗] SSH directory: NOT FOUND");
             };
+        } else {
+            println!("SSH version Error!");
+        }
     };
 
     
@@ -77,7 +102,7 @@ fn read_ssh_dir() -> Result<Vec<DirEntry>, Error> {
 
 // List SSH keypairs in `~/.ssh/`; return any I/O errors.
 fn list_keypairs() -> Result<(), Error> {
-    let ssh_dir: Vec<DirEntry> = read_ssh_dir()?;
+    let ssh_read: Vec<DirEntry> = read_ssh_dir()?;
     
     let key_names: Vec<String> = todo!(
         "filter ssh directory's entries for any pairs of files matching ssh's keypair file \
@@ -115,10 +140,10 @@ fn gen_rsa_keypair(name: &str, password: Option<&str>, comment: Option<&str>) ->
     
     
     match check {
-        0 => {arguments = vec!["-t ","rsa ","-P ","-b 4096 ","-q","[{name}] "];},
-        1 => {arguments = vec!["-t ","rsa ","-p {password} ","-b 4096 ","-q","[{name}] "];},
-        2 => {arguments = vec!["-t ","rsa ","-P ","-b 4096 ","-q","[{name}] ","-c {comment}"];},
-        3 => {arguments = vec!["-t ","rsa ","-p {password} ","-b 4096 ","-q","[{name}] ","-c {comment}"];}
+        0 => {arguments = vec!["-t","rsa","-P","-b 4096 ","-q","[{name}]"];},
+        1 => {arguments = vec!["-t","rsa","-p {password}","-b 4096","-q","[{name}]"];},
+        2 => {arguments = vec!["-t","rsa","-P ","-b 4096","-q","[{name}]","-c {comment}"];},
+        3 => {arguments = vec!["-t","rsa","-p {password}","-b 4096","-q","[{name}]","-c {comment}"];}
         _ => {println!("error building keygen arguments") }
     }
     let generate  = Command::new("ssh-keygen").args(arguments).output()
@@ -144,10 +169,10 @@ fn gen_ed_keypair(name: &str, password: Option<&str>, comment: Option<&str>) -> 
     
     
     match check {
-        0 => {arguments = vec!["-t ","ed25519 ","-P ","-q","[{name}] "];},
-        1 => {arguments = vec!["-t ","ed25519 ","-p {password} ","-b 4096 ","-q","[{name}] "];},
-        2 => {arguments = vec!["-t ","ed25519 ","-P ","-b 4096 ","-q","[{name}] ","-c {comment}"];},
-        3 => {arguments = vec!["-t ","ed25519 ","-p {password} ","-b 4096 ","-q","[{name}] ","-c {comment}"];}
+        0 => {arguments = vec!["-t ","ed25519 ","-P","-q","[{name}]"];},
+        1 => {arguments = vec!["-t ","ed25519 ","-p {password}","-b 4096","-q","[{name}]"];},
+        2 => {arguments = vec!["-t ","ed25519 ","-P ","-b 4096","-q","[{name}]","-c {comment}"];},
+        3 => {arguments = vec!["-t ","ed25519 ","-p {password}","-b 4096","-q","[{name}]","-c {comment}"];}
         _ => {println!("error building keygen arguments") }
     }
     let generate  = Command::new("ssh-keygen").args(arguments).output()
@@ -162,7 +187,7 @@ fn gen_ed_keypair(name: &str, password: Option<&str>, comment: Option<&str>) -> 
 fn show_pubkey(name: &str) -> Result<(), Error> {
     //todo!("use `read_file` to print key file's contents");
     //let ssh_dir = SSH_DIR;
-    let pubkey = read_file(SSH_DIR.to_owned() + "/"+ name +".pub").map_err(|error|{
+    let pubkey = read_file(SSH_DIR.to_owned() + "/"+ "{name}" +".pub").map_err(|error|{
         format!("failed to read public key`, error: `{error}`")
     })?;
     //let readkey = pubkey.trim();
@@ -172,17 +197,38 @@ fn show_pubkey(name: &str) -> Result<(), Error> {
 // Delete SSH keypair files from `~/.ssh/`; forward any I/O errors.
 fn delete_keypair(name: &str) -> Result<(), Error> {
     //todo!("use `remove_file` to remove ssh keypair files");
-    remove_file(SSH_DIR.to_owned() + "/"+ name +".pub").map_err(|error|{
+    remove_file(SSH_DIR.to_owned() + "/"+ "{name}" +".pub").map_err(|error|{
         format!("failed to read public key`, error: `{error}`")
     })?;
-    remove_file(SSH_DIR.to_owned() + "/"+ name).map_err(|error|{
+    remove_file(SSH_DIR.to_owned() + "/"+ "{name}").map_err(|error|{
         format!("failed to read public key`, error: `{error}`")
     })?;
     Ok(println!("Key pair deleted"))
 }
 
+//fn get_ssh_dir() -> Result<String,Error>{
+    //let mut ssh_dir_string = "";
+
+ //   let ssh_dir_output= Command::new("dir")
+ //   .args([".ssh","/AD","/s"])
+ //   .output()
+//    .map_err(|err| {
+//        format!("failed to run process: `dir`, error: `{err}`")
+ //   })?;
+
+ //   let sshd_output = String::from_utf8(ssh_dir_output.stderr)
+  //          .map_err(|error| format!("failed to run process: `dir`, error: `{error}`"))?;
+
+
+    //return ssh_dir_string.to_string();
+ //   Ok(sshd_output)
+//}
+
 fn main() -> Result<(), Error> {
+    //let user_ssh_dir:String = get_ssh_dir()?;
+// get .ssh dir and pass to functions <<<<<<<<<<<<<<<<<<<
     let ssh_installed = get_ssh_info()?;
+
     if !ssh_installed {
         return Ok(println!("ssh not installed!"));
     }
@@ -201,12 +247,12 @@ fn main() -> Result<(), Error> {
     list_keypairs()?;
     
     // Print public keys.
-    show_pubkey(rsa_key_name)?;
-    show_pubkey(ed_key_name)?;
+    show_pubkey(rsa_key_name, )?;
+    show_pubkey(ed_key_name, )?;
         
     // Delete generated keypairs.
-    delete_keypair(rsa_key_name)?;
-    delete_keypair(ed_key_name)?;
+    delete_keypair(rsa_key_name, )?;
+    delete_keypair(ed_key_name, )?;
 
     return Ok(println!("process complete."))
 }
